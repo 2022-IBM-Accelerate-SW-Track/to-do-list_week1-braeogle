299e97de0dcdb11756437cec24b47905ee725e5b
Brae Ogle <braeogle1@gmail.com>
2022-06-08 22:34:43 -0500

    final1
    Hopeing this is the correct result. No sure how to test is though

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import <brae> from; "../assets/brae.jpg"; //future PROBLEM
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              // Image goes here
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Brae Ogle</div>
+            <div className="brief_description">
+              My disription of myself. Im cool
+            </div>
+          </div>
+        </div>
       </div>
+      </div>
+    
     )
-  }
+   }
 }
